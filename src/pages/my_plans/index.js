a8131daa36d80dc70e7fa99bc0822a5b191147af
JoãoPedroZs <85975383+JP-Programador@users.noreply.github.com>
2023-11-26 23:04:57 -0300

    Update index.js

--- src/pages/my_plans/index.js
+++ src/pages/my_plans/index.js
@@ -12,7 +12,7 @@ import Reparos from '../../assets/Reparos.jpg'
 import Roubo from '../../assets/Roubo de bens.jpg'
 import Serviços from '../../assets/computadores.png'
 
-import { useState, version } from "react";
+import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
